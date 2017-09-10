7f5a155bf4c4382da723209bbec405c0f2378d81
Le Sauce Julien <julien.le_sauce@nokia.com>
2017-09-10 14:16:40 +0200

    Fix: Fixed FileUtils.copyFolder to create also parent folder path

--- src/main/java/org/jls/toolbox/util/file/FileUtils.java
+++ src/main/java/org/jls/toolbox/util/file/FileUtils.java
@@ -36,104 +36,114 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Utility class that peforms useful operations on files and directories.
+ * Utility class that performs operations on files and directories.
  * 
  * @author LE SAUCE Julien
  * @date Sep 3, 2015
  */
 public class FileUtils {
 
-	private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
-	/**
-	 * Delete a file or a directory, allowing recursive directory deletion.
-	 * Improved version of the {@link File#delete()} method.
-	 * 
-	 * @param file
-	 *            The file or directory to delete.
-	 * @param recursive
-	 *            <code>true</code> for a recursive deletion, <code>false</code>
-	 *            to only delete the specified file or directory.
-	 * @return <code>true</code> if the specified file or directory has been
-	 *         correctly deleted, <code>false</code> otherwise.
-	 */
-	public static boolean delete (final File file, final boolean recursive) {
-		if (file.exists()) {
-			// If it's a file or recursive option is not selected
-			if (!file.isDirectory() || !recursive) {
-				return file.delete();
-			}
-			File[] subFiles = file.listFiles();
-			// Iterates over the existing files
-			for (File subFile : subFiles) {
-				boolean deleted = delete(subFile, recursive);
-				if (!deleted) {
-					return false;
-				}
-			}
-			return file.delete();
-		} else {
-			return false;
-		}
-	}
+    /**
+     * Delete a file or a directory, allowing recursive directory deletion. Improved
+     * version of the {@link File#delete()} method.
+     * 
+     * @param file
+     *            The file or directory to delete.
+     * @param recursive
+     *            <code>true</code> for a recursive deletion, <code>false</code> to
+     *            only delete the specified file or directory.
+     * @return <code>true</code> if the specified file or directory has been
+     *         correctly deleted, <code>false</code> otherwise.
+     */
+    public static boolean delete(final File file, final boolean recursive) {
+        if (file.exists()) {
+            // If it's a file or recursive option is not selected
+            if (!file.isDirectory() || !recursive) {
+                return file.delete();
+            }
+            File[] subFiles = file.listFiles();
+            // Iterates over the existing files
+            for (File subFile : subFiles) {
+                boolean deleted = delete(subFile, recursive);
+                if (!deleted) {
+                    return false;
+                }
+            }
+            return file.delete();
+        } else {
+            return false;
+        }
+    }
 
-	/**
-	 * Copies recursively the specified source folder to the specified
-	 * destination. If the destination does not exist then the destination
-	 * directory is created.
-	 * 
-	 * @param src
-	 *            Source folder to copy and paste.
-	 * @param dst
-	 *            Destination folder.
-	 * @throws IOException
-	 *             If an error occurred copying the files to the specified
-	 *             destination.
-	 */
-	public static void copyFolder (final File src, final File dst) throws IOException {
-		// Checks input
-		if (src == null) {
-			throw new NullPointerException("Source cannot be null");
-		}
-		if (dst == null) {
-			throw new NullPointerException("Destination cannot be null");
-		}
-		if (!src.exists()) {
-			throw new FileNotFoundException("Source file not found : " + src.getAbsolutePath());
-		}
-		if (dst.getParentFile() != null && !dst.getParentFile().exists()) {
-			throw new FileNotFoundException("Destination parent not found : " + dst.getParentFile().getAbsolutePath());
-		}
+    /**
+     * Copies recursively the specified source folder to the specified destination.
+     * If the destination does not exist then the destination directory is created.
+     * 
+     * @param src
+     *            Source folder to copy and paste.
+     * @param dst
+     *            Destination folder.
+     * @throws IOException
+     *             If an error occurred copying the files to the specified
+     *             destination.
+     */
+    public static void copyFolder(final File src, final File dst) throws IOException {
+        // Checks input
+        if (src == null) {
+            throw new NullPointerException("Source cannot be null");
+        }
+        if (dst == null) {
+            throw new NullPointerException("Destination cannot be null");
+        }
+        if (!src.exists()) {
+            throw new FileNotFoundException("Source file not found : " + src.getAbsolutePath());
+        }
 
-		// If the source is a directory
-		if (src.isDirectory()) {
-			// If destination does not exist
-			if (!dst.exists()) {
-				// Creates the directory
-				dst.mkdir();
-				LOGGER.debug("Directory copied from {} to {}", src, dst);
-			}
-			// Lists the content
-			String[] files = src.list();
-			// Construct the src and dst file structure
-			for (String filename : files) {
-				// Construct src and dst files
-				File srcFile = new File(src, filename);
-				File dstFile = new File(dst, filename);
-				// Recursive copy
-				copyFolder(srcFile, dstFile);
-			}
-		} else { // src is a file
-			// then copy it
-			try (InputStream in = new FileInputStream(src); OutputStream out = new FileOutputStream(dst)) {
-				byte[] buffer = new byte[1024];
-				int length;
-				// Copy the content
-				while ((length = in.read(buffer)) > 0) {
-					out.write(buffer, 0, length);
-				}
-			}
-			LOGGER.debug("File copied from {} to {}", src, dst);
-		}
-	}
+        // If the source is a directory
+        if (src.isDirectory()) {
+            // If destination does not exist
+            if (!dst.exists()) {
+                createDirectory(dst);
+                LOGGER.debug("Directory copied from {} to {}", src, dst);
+            }
+            // Lists the content
+            String[] files = src.list();
+            // Construct the src and dst file structure
+            for (String filename : files) {
+                // Construct src and dst files
+                File srcFile = new File(src, filename);
+                File dstFile = new File(dst, filename);
+                // Recursive copy
+                copyFolder(srcFile, dstFile);
+            }
+        } else { // src is a file
+            // then copy it
+            try (InputStream in = new FileInputStream(src); OutputStream out = new FileOutputStream(dst)) {
+                byte[] buffer = new byte[1024];
+                int length;
+                // Copy the content
+                while ((length = in.read(buffer)) > 0) {
+                    out.write(buffer, 0, length);
+                }
+            }
+            LOGGER.debug("File copied from {} to {}", src, dst);
+        }
+    }
+
+    /**
+     * Create the specified directory (if the parent folders doesn't exist, then
+     * those folders will be created).
+     * 
+     * @param dst
+     *            The directory to create.
+     * @throws FileNotFoundException
+     *             If folder creation failed.
+     */
+    private static void createDirectory(final File dst) throws FileNotFoundException {
+        if (!dst.mkdirs()) {
+            throw new FileNotFoundException("Failed to create folder " + dst.getAbsolutePath());
+        }
+    }
 }
